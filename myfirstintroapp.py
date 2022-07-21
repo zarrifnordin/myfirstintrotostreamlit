3af7b23b59b3b5116f55a5903e7cf54bb02de807
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-21 12:29:46 +0800

    Update myfirstintroapp.py

--- myfirstintroapp.py
+++ myfirstintroapp.py
@@ -15,3 +15,15 @@ st.write(pd.DataFrame({
     'Intplan': ['yes', 'yes', 'yes', 'no'],
     'Churn Status': [0, 0, 0, 1]
 }))
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("My first Streamlit App")
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
